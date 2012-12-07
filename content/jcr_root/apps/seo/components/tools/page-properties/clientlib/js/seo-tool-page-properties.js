9069e3721735a4350199cf622f835f0ed7d9891e
Julian Sedding <julian.sedding@code-distillery.net>
2012-12-07 13:53:10 +0100

    rename titles plugin to  page-properties

---
+++ content/jcr_root/apps/seo/components/tools/page-properties/clientlib/js/seo-tool-page-properties.js
@@ -0,0 +1,42 @@
+;(function($, window, document, console) {
+    $(document).on("seo-toolbox-ready", function(event, toolbox) {
+        var inputFields = $(toolbox).find(".tools .page-properties [data-properties]");
+        /*
+            .on("click", function(event) {
+                $(event.currentTarget).attr("contenteditable", true);
+            });
+        */
+        $(toolbox).on("change", ".tools .page-properties [data-properties]", function(event) {
+            var field = $(event.currentTarget);
+            var propName = field.attr("data-properties").split(",")[0];
+            var params = {};
+            params[propName] = field.val();
+            $.post(CQ.WCM.getPagePath() + "/jcr:content", params);
+        });
+        var jsonURL = $(toolbox).attr("data-base-path");
+        $.get(jsonURL, { page: CQ.WCM.getPagePath()}, function(pageProps) {
+            inputFields.each(function(idx, field) {
+                var propNames = $(field).attr("data-properties").split(",");
+                var value;
+                for (var i = 0; i < propNames.length; i++) {
+                    var propName = propNames[i];
+                    value = pageProps[propName];
+                    if (value && value !== "") {
+                        break;
+                    }
+                }
+
+                if (value) {
+                    $(field).val(value);
+                }
+            });
+        });
+        console.debug("Initializing titles plugin", toolbox);
+    });
+    $(document).on("seo-toolbox-beforeshow", function(event, toolbox) {
+        console.debug("Initializing titles plugin beforeshow", toolbox);
+    });
+    $(document).on("seo-toolbox-beforehide", function(event, toolbox) {
+        console.debug("Initializing titles plugin beforehide", toolbox);
+    });
+})($CQ, window, document, CQ.WCM.getTopWindow().console);
---
+++ content/jcr_root/apps/seo/components/tools/page-properties/seo.json.jsp
@@ -0,0 +1,36 @@
+<%@page session="false" contentType="application/json" pageEncoding="utf-8" %><%
+%><%@page import="
+    org.apache.sling.commons.json.JSONObject,
+    org.apache.commons.lang.StringUtils
+" %><%@include file="/libs/foundation/global.jsp"%><%
+    final String[] PROPERTY_NAMES = {
+            "urlHandleProperties",
+            "pageTitleProperties",
+            "pageHeadingProperties",
+            "navigationTitleProperties",
+            "descriptionProperties"
+    };
+
+    final JSONObject json = new JSONObject();
+    final String analyzedPagePath = slingRequest.getParameter("page");
+    final Page analyzedPage = pageManager.getPage(analyzedPagePath);
+    if (analyzedPage != null) {
+        final ValueMap analyzedPageProperties = analyzedPage.getProperties();
+        for (final String name : PROPERTY_NAMES) {
+            final String value = properties.get(name, "");
+            final String[] pagePropNames = StringUtils.split(value, ',');
+            for (final String pagePropName : pagePropNames) {
+                final String pagePropValue;
+                if (":nodename".equals(pagePropName)) {
+                    pagePropValue = analyzedPage.getName();
+                } else {
+                    pagePropValue = analyzedPageProperties.get(pagePropName, String.class);
+                }
+                if (pagePropValue != null) {
+                    json.put(pagePropName, pagePropValue);
+                }
+            }
+        }
+    }
+    response.setContentType("application/json");
+%><%=json.toString()%>
